51c271b5b03a0b064c6fafdab0bf632e189a2f05
Anja Stene <anja.stene@nmbu.no>
2020-06-08 14:27:55 +0200

    Class for handling all cells on island, implemented methods for annual cycle except migrations

--- biosim/island.py
+++ biosim/island.py
@@ -1,10 +1,4 @@
-from biosim.animal import Animal
-import numpy as np
-from numpy import random
-
-
-class BioSim:
-    pass
+from biosim.cell import Cell, Lowland
 
 
 class Island:
@@ -23,139 +17,93 @@ class Island:
     #    for cell in cell_list:
     #        cell.set_fodder()
 
+    def create_new_cell(self):
+        c = Lowland()  #Later replaced by function creating different types of land
+        self.cell_list.append(c)
+
     def feed_animals(self):
-        for cell in cell_list:
+        for cell in self.cell_list:
             cell.animals_eat()
 
     def procreation(self):
-        pass
+        for cell in self.cell_list:
+            cell.procreation()
+
+    def aging(self):
+        for cell in self.cell_list:
+            cell.animals_age_by_one_year()
+
+    def death(self):
+        for cell in self.cell_list:
+            cell.animals_die()
+
+    def loose_weight(self):
+        #Every year, the weight of the animal decreases by eta * omega.
+        for cell in self.cell_list:
+            cell.animals_update_weight_annual()
 
     def migration(self):
         pass
 
-    def aging(self):
-        pass
-
-    def loose_weight(self):
-        pass
-
-    def death(self):
-        pass
 
     # def eating for every cell
 
 
-class Cell:
-    """
-    Creating cell structure to describe properties in every square (cell)
-    Common for all cells:
-     - coordinates
-     - edge_cells for migration
-     - animals
-     - fodder_amount
-     -
-
-    :return:
-    """
-
-    def __init__(self):
-        self.animal_list = []
-        self.available_fodder = 0
-
-    def place_animals(self):
-        pass
-
-    def randomise_list(self):
-        rnd_list = self.animal_list.copy()
-        np.random.shuffle(rnd_list)
-        return rnd_list
-
-    def animals_eat(self):  # herbivore feeding
-        randomized_order = self.randomise_list()
-        for animal in randomized_order:
-            if self.available_fodder >= animal.get_F():
-                self.available_fodder -= animal.eat()
-
-    def procreation(self):
-        """
-        Checks if there are at least one other animal of the same species in this cell
-        and having proper weight for both
-        then they will make a new offspring.
-        Returns
-        -------
-
-        """
-        for animal in self.animal_list:
-            newborn_weight = animal.get_initial_weight()
-            if animal.check_if_will_create_newborn(len(self.animal_list), newborn_weight):
-                self.create_new_animal(newborn_weight)
-
-    def create_new_animal(self, newborn_weight):
-        new_animal = Animal(weight=newborn_weight)
-        self.animal_list.append(new_animal)
-
-    def animal_death(self):
-        pass
-
-    def migrate(self):
-        pass
-
-    def get_fodder(self):
-        return self.available_fodder
-
-    def get_num_animals(self):
-        return len(self.animal_list)
-
-
-class Water(Cell):
-    def __init__(self):
-        self.animals = []
-        self.available_fodder = 0
-
-    def set_fodder(self):
-        self.available_fodder = 0
-
-
-class Desert(Cell):
-    def __init__(self):
-        self.animals = []
-        self.available_fodder = 0
-
-    def set_fodder(self):
-        self.available_fodder = 0
-
-
-class Highland(Cell):
-    def __init__(self):
-        self.animals = []
-        self.available_fodder = 300
-
-    def set_fodder(self):
-        self.available_fodder = 300
-
-
-class Lowland(Cell):
-    def __init__(self):
-        super().__init__()
-        self.animals = []
-        self.available_fodder = 800
-
-    def set_fodder(self):
-        self.available_fodder = 800
-
 
 if __name__ == "__main__":
-    a=Animal()
-    b=Animal()
-    c=Lowland()
-    c.animal_list.append(a)
-    c.animal_list.append(b)
 
-    print(c.get_fodder())
-    for _ in range(30):
-        c.animals_eat()
-    print(c.get_fodder())
 
-    print(c.get_num_animals())
-    c.procreation()
-    print(c.get_num_animals())
+    i = Island()
+    i.create_new_cell()
+
+    for cell in i.cell_list:
+        cell.place_animals()
+        cell.place_animals()
+
+    for cell in i.cell_list:
+        print("fodder:", cell.get_fodder())
+        for animal in cell.animal_list:
+            print("weight:", animal.get_weight())
+
+    for _ in range(20):
+        i.feed_animals()
+
+    for cell in i.cell_list:
+        print("fodder:", cell.get_fodder())
+        for animal in cell.animal_list:
+            print("weight after eating:", animal.get_weight())
+
+    for cell in i.cell_list:
+        print("number of animals in cell", cell.get_num_animals())
+
+    i.procreation()
+
+    for cell in i.cell_list:
+        print("number of animals after procreation", cell.get_num_animals())
+
+    for cell in i.cell_list:
+        for animal in cell.animal_list:
+            print("age: ", animal.get_age())
+
+    i.aging()
+
+    for cell in i.cell_list:
+        for animal in cell.animal_list:
+            print("age after aging: ", animal.get_age())
+
+    for cell in i.cell_list:
+        for animal in cell.animal_list:
+            print("weight: ", animal.get_weight())
+
+    i.loose_weight()
+
+    for cell in i.cell_list:
+        for animal in cell.animal_list:
+            print("weight after loosing: ", animal.get_weight())
+
+
+    for cell in i.cell_list:
+        print("number of animals in cell", cell.get_num_animals())
+    i.death()
+    for cell in i.cell_list:
+        print("number of animals after killing", cell.get_num_animals())
