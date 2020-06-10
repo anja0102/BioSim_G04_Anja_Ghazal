8d61bf287d4934bea87e53ae73cbda83ba31f376
Anja Stene <anja.stene@nmbu.no>
2020-06-10 11:57:01 +0200

    carnivore adaptations

--- biosim/simulationfile.py
+++ biosim/simulationfile.py
@@ -3,6 +3,9 @@ import numpy as np
 np.random.seed(1)
 
 listof = [{'species': 'Herbivore', 'age': 0, 'weight': 20} for _ in range(50)]
+listof2 = [{'species': 'Carnivore', 'age': 0, 'weight': 100} for _ in range(10)]
+
+listof.extend(listof2)
 
 i = Island()
 i.create_new_cell()
@@ -11,13 +14,16 @@ for cell in i.cell_list:
     cell.place_animals(listof)
 
 for cell in i.cell_list:
-    print(  len(cell.herbivores_list))
+    print(  len(cell.carnivores_list))
 
-num_years = 200
+num_years = 50
 
-num_animals_everyyear=[]
+num_herb_animals_everyyear=[]
+num_carn_animals_everyyear=[]
+
 fitness_of_one_animal = []
 for year in range(num_years):
+    print(year)
     i.grow_fodder()
     i.feed_animals()
     # for cell in i.cell_list:
@@ -35,10 +41,12 @@ for year in range(num_years):
     i.aging()
 
     for cell in i.cell_list:
-        num_animals_everyyear.append(cell.get_num_animals())
+        num_herb_animals_everyyear.append(cell.get_num_herb_animals())
+        num_carn_animals_everyyear.append(cell.get_num_carn_animals())
 
-print(num_animals_everyyear)
-print(fitness_of_one_animal)
+print(num_herb_animals_everyyear)
+print(num_carn_animals_everyyear)
+
 
 # import matplotlib.pyplot as plt
 # plt.plot( list( range(len(fitness_of_one_animal))), fitness_of_one_animal,'--' )
