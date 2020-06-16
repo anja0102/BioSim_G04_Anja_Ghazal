127bc84fe4a909136b210b0d5766f73871ec4659
Ghazal Azadi <ghaz@nmbu.no>
2020-06-16 12:07:35 +0200

    adding / changing some parts of the class for visualisation

--- biosim/simulation.py
+++ biosim/simulation.py
@@ -2,6 +2,16 @@ from biosim.animal import Carnivore, Herbivore
 from biosim.cell import Highland, Lowland
 from biosim.island import Island
 import numpy as np
+import pandas as pd
+from biosim.visualisation import Visualisation
+import matplotlib.pyplot as plt
+import matplotlib
+import os
+
+_DEFAULT_GRAPHICS_DIR = os.path.join('../results', '')
+_DEFAULT_GRAPHICS_NAME = 'biosim'
+_DEFAULT_MOVIE_FORMAT = 'mp4'
+
 
 class BioSim:
 
@@ -14,17 +24,24 @@ class BioSim:
                 cmax_animals=None,
                 hist_specs=None,
                 img_base=None,
-                img_fmt="png",
+                img_fmt="png"
     ):
 
         self._animal_species = {'Carnivore': Carnivore, 'Herbivore': Herbivore}
         self._landscapes_with_changeable_parameters = {'H': Highland, 'L': Lowland}
+        self._island_map = island_map
         self._island = Island(island_map)
+        self._vis = None
+        self._fig = None
         self.add_population(ini_pop)
+        self._final_year = None
+        self._year = 0
         np.random.seed(seed)
+        self.maximum = 0
 
         if ymax_animals is None:  #the y-axis limit should be adjusted automatically.
-            self.ymax_animals = None # or call a function to get max num animals updated?
+            # matplotlib.pyplot.autoscale(enable=True, axis='both', tight=None)
+            self.ymax_animals = 20000  # or call a function to get max num animals updated?
         else:
             self.ymax_animals = ymax_animals
 
@@ -32,6 +49,13 @@ class BioSim:
             self._cmax_animals = {'Herbivore': 5, 'Carnivore': 5}
         else:
             self._cmax_animals = cmax_animals
+
+        if img_base is None:
+            self._img_base = _DEFAULT_GRAPHICS_DIR + _DEFAULT_GRAPHICS_NAME
+        else:
+            self._img_base = img_base
+        self._img_ctr = 0
+        self._img_fmt = img_fmt
 
 
     """
@@ -109,9 +133,84 @@ class BioSim:
         if img_years is None:
             img_years = vis_years
 
-        for year in range(num_years):
+        self._final_year = self._year + num_years
+        self._setup_graphics()
+
+        while self._year < self._final_year:
+
+            if self._year % vis_years == 0:
+                self._update_graphics()
+
+            if self._year % img_years == 0:
+                self._save_graphics()
+
             self._island.annual_cycle()
+            self._year += 1
+
+            print("Year " + str(self._year))
             print(self.num_animals_per_species)
+
+    def _save_to_csv(self):
+        pass
+    #     df = self._animal_distribution
+    #     df.to_csv('../results/data.csv', sep='\t', encoding='utf-8')
+
+
+    def y_max(self):
+        """
+        Maximum number of animals
+        """
+        new_value = self.num_animals
+        if new_value > self.maximum:
+            self.maximum = new_value
+        return self.maximum
+
+    def _setup_graphics(self):
+        """
+        Creates subplots.
+        """
+        map_dims = self._island.cells_dims
+
+        if self._fig is None:
+            self._fig = plt.figure()
+            self._vis = Visualisation(self._island_map, self._fig, map_dims)
+
+        self._vis.visualise_map()
+        self._vis.animal_graphs(self._final_year, self.ymax_animals)
+
+        self._vis.animal_dist_graphs()
+        self._fig.tight_layout()
+
+    def _update_graphics(self):
+        """
+        Updates graphics with current data.
+        """
+        df = self._animal_distribution
+        rows, cols = self._island.cells_dims
+        dist_matrix_carnivore = np.array(df[['Carnivore']]).reshape(rows, cols)
+        dist_matrix_herbivore = np.array(df[['Herbivore']]).reshape(rows, cols)
+        self._update_animals_graph()
+        self._vis.update_herbivore_dist(dist_matrix_herbivore)
+        self._vis.update_carnivore_dist(dist_matrix_carnivore)
+        plt.pause(1e-6)
+        self._fig.suptitle('Year: ' + str(self.year), x=0.5)
+
+    def _update_animals_graph(self):
+        herb_count, carn_count = list(self.num_animals_per_species.values())
+        self._vis.update_graphs(self._year, herb_count, carn_count)
+
+    def _save_graphics(self):
+        """
+        Saves graphics to file if file name is given.
+        """
+        pass
+        # if self._img_base is None:
+        #     return
+        #
+        # plt.savefig('{base}_{num:05d}.{type}'.format(base=self._img_base,
+        #                                              num=self._img_ctr,
+        #                                              type=self._img_fmt))
+        # self._img_ctr += 1
 
 
     def add_population(self, population):
@@ -123,20 +222,25 @@ class BioSim:
         """
         self._island.place_animals(population)
 
-
     @property
     def year(self):
-
-
-        """Last year simulated."""
-
+        """
+        Simulates last year.
+        """
+        return self._year
 
     @property
     def num_animals(self):
-
-
-        """Total number of animals on island."""
-
+        """
+        Calculates total number of animals on island.
+        Returns
+        -------
+        total_num: int
+        """
+        total_num = 0
+        for species in self._animal_species:
+            total_num += self._island.total_num_animals_per_species(species)
+        return total_num
 
 
     @property
@@ -155,6 +259,28 @@ class BioSim:
 
 
     def make_movie(self):
-
-
         """Create MPEG4 movie from visualization images saved."""
+        pass
+
+
+
+
+    @property
+    def _animal_distribution(self):
+        """
+        Calculates Pandas DataFrame with animal count per species for each cell
+        on island.
+        Returns
+        -------
+        pd.DataFrame(count_df): data frame
+        """
+        count_df = []
+        rows, cols = self._island.cells_dims
+        for i in range(rows):
+            for j in range(cols):
+                cell = self._island.get_cells()[i, j]
+                animals_count = cell.cell_fauna_count
+                count_df.append({'Row': i, 'Col': j,
+                                 'Herbivore': animals_count['Herbivore'],
+                                 'Carnivore': animals_count['Carnivore']})
+        return pd.DataFrame(count_df)
