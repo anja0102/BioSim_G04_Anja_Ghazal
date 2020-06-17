f85ca1d888d35c69071db35e589097e07cdb483a
Anja Stene <anja.stene@nmbu.no>
2020-06-17 17:34:48 +0200

    Docstrings and pep8 DONE

--- biosim/island.py
+++ biosim/island.py
@@ -1,36 +1,54 @@
-from biosim.cell import Cell, Lowland, Highland, Water, Desert
+# -*- coding: utf-8 -*-
+
+__author__ = "Anja Stene, Student NMBU", "Ghazal Azadi, Student NMBU"
+__email__ = "anja.stene@nmbu.no", "ghazal.azadi@nmbu.no"
+
+from biosim.cell import Lowland, Highland, Water, Desert
 from biosim.animal import Carnivore, Herbivore
 import numpy as np
 
 
 class Island:
     """
-    - age
-    - cells of type "celltype"
-    - animals (inside celltype)
-    -
-    :return:
+    Island class created by a map (string). Consists of Cell objects of different types.
     """
 
     def __init__(self, island_map):
+        """
+        Constructor class for the Island
+        Parameters
+        ----------
+        island_map: string
+        """
         self._map = island_map
         self._island_map = self._string_to_np_array()
         self._not_surrounded_by_ocean(self._island_map)
+
         self._landscape_classes = {'W': Water,
                                    'L': Lowland,
                                    'H': Highland,
                                    'D': Desert}
         self._animal_classes = {'Carnivore': Carnivore,
                                 'Herbivore': Herbivore}
+
         self._cells = self.create_map_of_landscape_objects()
         rows = self._cells.shape[0]
         cols = self._cells.shape[1]
         self.cells_dims = rows, cols
 
     def get_cells(self):
+        """
+        Returns all cells on Island
+        Returns
+        -------
+        _cells: all cell objects in numpy.ndarray
+        """
         return self._cells
 
     def annual_cycle(self):
+        """
+        Annual Cycle of happenings on the Island
+        """
         self.grow_fodder()
         self.feed_animals()
         self.procreation()
@@ -49,8 +67,9 @@ class Island:
 
         Returns
         -------
-        map_edges: list
+        map_edges: list of cell objects
         """
+
         rows, cols = map_array.shape[0], map_array.shape[1]
         map_edges = [map_array[0, :cols], map_array[rows - 1, :cols],
                      map_array[:rows - 1, 0], map_array[:rows - 1, cols - 1]]
@@ -72,7 +91,9 @@ class Island:
 
     def _string_to_np_array(self):
         """
-        Converts string to numpy array with the same diemsions.
+        Converts string map to numpy array with the same dimensions.
+        Raises ValueError if dimensions not valid
+
         Returns
         -------
         char_map: np.ndarray
@@ -95,6 +116,7 @@ class Island:
         iterate through the given character array and build the object of
         landscapes for each character. Afterwards, save the landscape class and
         instantiate the object.
+
         Returns
         -------
         cells_array: np.ndarray of landscape objects
@@ -108,14 +130,17 @@ class Island:
 
     def _create_cell(self, cell_letter):
         """
-        create cell object based on given string
+        Create cell object based on given character, each representing a cell-type.
+
         Parameters
         ----------
         cell_letter: str
+
         Returns
         -------
-        class of landscape ()
+        object: cell object of correct landscape type
         """
+
         if cell_letter in self._landscape_classes:
             return self._landscape_classes[cell_letter]()
         else:
@@ -124,14 +149,18 @@ class Island:
     def _adj_cells(self, x, y):
         """
         Returns the list of 4 adjacent cells.
+
         Parameters
         ----------
         x: int
+            current x-coordinate
         y: int
+            current y-coordinate
+
         Returns
         -------
         adj_cells_list: list
-            List of 4 adjacent cells
+            List of 4 cell objects representing the adjacent cells
         """
         rows, cols = self.cells_dims
         adj_cells_list = []
@@ -146,17 +175,32 @@ class Island:
         return adj_cells_list
 
     def get_adj_cells(self, x, y):
+        """
+        Gets the four adjacent cells of current location (x,y)
+        Parameters
+        ----------
+        x: int
+        y: int
+
+        Returns
+        -------
+        list of adjacent cells (4 objects)
+        """
+
         return self._adj_cells(x, y)
 
     def total_num_animals_per_species(self, species):
         """
-        Calculates number of animals per kind for all cells per species.
+        Calculates total number of animals per kind on Island.
+
         Parameters
         ----------
         species: str
+
         Returns
         -------
-        num_animals: dict
+        num_animals: int
+            total number of animals of specified specie on Island
         """
         num_animals = 0
         rows, cols = self.cells_dims
@@ -167,8 +211,16 @@ class Island:
         return num_animals
 
     def place_animals(self, ini_animals):
-        for dct in ini_animals:
+        """
+        Places population of animals on Island in correct location.
+        If location is not given, then ValueError is raised.
+        The upper left most cell has coordinates (1,1)
 
+        Parameters
+        ----------
+        ini_animals: list of dicts
+        """
+        for dct in ini_animals:
             if dct.get('loc') is None:
                 raise ValueError('Location of animal to place was not given')
 
@@ -179,6 +231,9 @@ class Island:
             self._cells[x, y].place_animals(dct.get('pop'))
 
     def grow_fodder(self):
+        """
+        Calls the set_fodder method in all Cells that the Island consists of.
+        """
 
         rows, cols = self.cells_dims
         for x in range(rows):
@@ -186,6 +241,9 @@ class Island:
                 self._cells[x, y].set_fodder()
 
     def feed_animals(self):
+        """
+        Calls the animals_eat method in all Cells that the Island consists of.
+        """
 
         rows, cols = self.cells_dims
         for x in range(rows):
@@ -193,24 +251,41 @@ class Island:
                 self._cells[x, y].animals_eat()
 
     def procreation(self):
+        """
+        Calls the procreation method in all Cells that the Island consists of.
+        """
+
         rows, cols = self.cells_dims
         for x in range(rows):
             for y in range(cols):
                 self._cells[x, y].procreation()
 
     def aging(self):
+        """
+        Calls the animals_age_by_one_year method in all Cells that the Island consists of.
+        """
+
         rows, cols = self.cells_dims
         for x in range(rows):
             for y in range(cols):
                 self._cells[x, y].animals_age_by_one_year()
 
     def death(self):
+        """
+        Calls the animals_die method in all Cells that the Island consists of.
+        """
+
         rows, cols = self.cells_dims
         for x in range(rows):
             for y in range(cols):
                 self._cells[x, y].animals_die()
 
     def migration(self):
+        """
+        Handles migration on the Island. No animal can migrate to a Water cell.
+        Checks what animals will migrate, and to where.
+        """
+
         rows, cols = self.cells_dims
         for x in range(rows):
             for y in range(cols):
@@ -219,8 +294,21 @@ class Island:
                     self.move_migrated_animals(migration_dct, x, y)
 
     def move_migrated_animals(self, migration_dct, x, y):
-        herbi_to_remove=[]
-        carni_to_remove=[]
+        """
+        Moves the migrated animals by removing them from the current cell and adding to new cell.
+
+        Parameters
+        ----------
+        migration_dct: dict
+            of migrated animals and their new location cell
+        x: int
+            current x-coordinate
+        y: int
+            current y-coordinate
+        """
+
+        herbi_to_remove = []
+        carni_to_remove = []
 
         for cell in migration_dct.keys():
             if not type(cell).__name__ == "Water":
@@ -234,11 +322,3 @@ class Island:
                         carni_to_remove.append(animal)
 
         self._cells[x, y].remove_migrated_animals(herbi_to_remove, carni_to_remove)
-
-if __name__ == '__main__':
-
-    geogr = """\
-    WWWWW
-    WHHW
-    WWWWW"""
-
