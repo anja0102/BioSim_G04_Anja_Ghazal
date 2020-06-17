5bfafa55583fcf49bb2f9cb6e6e918af28839f95
Anja Stene <anja.stene@nmbu.no>
2020-06-17 10:00:01 +0200

    Docstrings and pep8 DONE

--- biosim/animal.py
+++ biosim/animal.py
@@ -4,47 +4,51 @@ import numpy as np
 
 class Animal:
     """
-    Creating the animal class
-    Fauna which are common among herbivores and carnivores are listed as methods in Animal
-    class
+    Animal class, where one object represents one animal
+    Has two subclasses of different animal species, Carnivores and Herbivores.
+    The most distinct difference between the two subclasses: the eating-method.
 
     """
     params = {}
 
     def __init__(self, age=None, weight=None):
         """
-        Constructor for the class, if weight or age initialized their value is set
-        if not, initial value for age would be 0 and for weight would be extracted
-        from the initial_weight method.
+        Constructor for the class, if weight or age initialized their value is set.
+        If no value is given;
+        initial value for age would be 0 and
+        initial value for weight is extracted from the initial_weight method.
+        Specifying invalid age or weight values will raise a a value error.
+        Initial fitness is set to zero.
+
         Parameters
         ----------
         age : int
-        weight : int
+        weight : float
 
         """
         if age is None:
             self.age = 0
         else:
-            # CHECK VALUE ERROR
+            self.raise_non_valid_attribute('Age', age)
             self.age = age
 
         if weight is None:
             self.weight = self.initial_weight()
         else:
-            # CHECK VALUE ERROR
+            self.raise_non_valid_attribute('Weight', weight)
             self.weight = weight
 
         self.has_migrated_this_year = False
 
-
     @classmethod
     def set_given_parameters(cls, parameters):
         """
-        save the user defined parameter value for Carnivore and Herbivore
-        inside the class variables parameters.
+        Set the user defined parameter values for Animal object.
+        Includes check for valid parameter values
+
         Parameters
         ----------
-        params: dict
+        parameters: dict
         """
 
         for parameter in parameters:
@@ -54,7 +58,7 @@ class Animal:
                                      'can\'t be more than 1')
                 if parameter == 'DeltaPhiMax' and parameters[parameter] <= 0:
                     raise ValueError('Illegal parameter value, ' +
-                                     str(parameter) + ' can\'t be or negative')
+                                     str(parameter) + ' can\'t be zero or negative')
                 if parameters[parameter] < 0:
                     raise ValueError('Illegal parameter value, ' +
                                      str(parameter) + ' can\'t be negative')
@@ -64,32 +68,12 @@ class Animal:
                 raise RuntimeError('Unknown parameter, ' + str(parameter) +
                                    ' can\'t be set')
 
-
-    # Move this down to herb class, since not used by carnivores
-    def eat(self, amount=None):
-        """
-        Herbivores eat based on the specific amount. If amount is initialized the value would
-        be set. If not its value will be chosen based on the standard parameters for herbivore
-        class
-        Parameters
-        ----------
-        amount : int
-
-        Returns
-        -------
-        amount of food the herbivore consumed.
-
-        """
-        if amount is None:
-            amount = self.params['F']
-
-        self.weight += self.params['beta'] * amount
-        return amount
-
     def grow_older(self):
         """
-        When the method is called the animal grows older for one year
-
+        Animal object grows one year older:
+        increasing age by 1,
+        updating the weight by annual weight loss,
+        setting the "has_migrated_this_year" value to False
         """
         self.age += 1
         self.update_weight("decrease")
@@ -98,24 +82,29 @@ class Animal:
     @classmethod
     def initial_weight(cls):
         """
-        initialize the weights based on the normal distribution
+        initialize the weights class attribute based on the normal distribution
 
         Returns
         -------
-        Initial weight
+        Initial weight: float
+            random number drawn from normal distribution
 
         """
-        rn = np.random.normal(cls.params['w_birth'], cls.params['sigma_birth'])
-        return rn
+
+        return np.random.normal(cls.params['w_birth'], cls.params['sigma_birth'])
 
     def update_weight(self, direction, amount=None):
         """
-        This method determines if the weight should either be decreased or increased.
-        In addition it updates the weight based on the parameters been given to.
+        This method updates the weight attribute.
+        If it should be decreased or increased is specified by "Direction".
+        The weight is updated based amount specified by argument.
+
         Parameters
         ----------
-        direction
-        amount : int
+        direction: string
+            direction of weight update (increase or decrease)
+        amount : float
+            amount eaten by the animal, which influences the weight update
 
         """
         if amount is None:
@@ -134,26 +123,36 @@ class Animal:
     def _fitness_formula(age, weight, params):
         """
         The formula which would be used to calculate the fitness
+        Computes fitness according to the formula:
+        Phi = q(-1, a, a_half, phi_age)*q(+1, w, w_half, phi_weight)
+        and 0=< Phi =< 1
+
         Parameters
         ----------
         age :  int
-        weight : int
-        params : int
+        weight : float
+        params : dict
+            class dictionary "params"
 
         Returns
         -------
-        Output of fitness formula
+        q1*q2: float
+            the fitness of the animal calculated from fitness formula
 
         """
-        q1 = 1 / (1 + math.exp(params['phi_age'] * (age - params['a_half'])))
+        q1 = 1 / (1 + math.exp(1 * params['phi_age'] * (age - params['a_half'])))
         q2 = 1 / (1 + math.exp(-1 * (params['phi_weight'] * (weight - params['w_half']))))
         return q1*q2
 
     def calculate_fitness(self):
         """
+        Calculate the fitness of an Animal. I weight is zero, fitness is always zero.
+        Else, fitness is calculated based on fitness formula.
 
         Returns
         -------
+        fitness: float
+            the fitness of the animal object
 
         """
         if self.weight == 0:
@@ -161,7 +160,21 @@ class Animal:
         else:
             return self._fitness_formula(self.age, self.weight, self.params)
 
-    def from_prob_to_binary(self, prob):
+    @staticmethod
+    def from_prob_to_binary(prob):
+        """
+        Converts a probability to a binary value (True or False).
+
+        Parameters
+        ----------
+        prob: float
+            between 0 and 1
+
+        Returns
+        -------
+        binary: boolean
+            True or False based on random number and probability
+        """
         rand_nbr = np.random.random()
         if rand_nbr < prob:
             return True
@@ -169,16 +182,43 @@ class Animal:
             return False
 
     def check_mating_weight_conditions(self, num_animals):
+        """
+        Method to check if animals weight conditions for mating is satisfied.
+
+        Parameters
+        ----------
+        num_animals: int
+            number of animals of same species in cell.
+            Mating partner exists if num_animals >= 2
+
+        Returns
+        -------
+        binary: boolean
+            True or False based on condition checks
+        """
         if num_animals >= 2:
             weight_condition = self.params['zeta'] * (self.params['w_birth'] + self.params['sigma_birth'])
             if self.weight > weight_condition:
                 return True
-            else:
-                return False
-        else:
-            return False
+        return False
 
     def check_mother_minus_newborn_weight_conditions(self, newborn_weight):
+        """
+        Method to check conditions for mating is satisfied.
+        Checking that mothers weight can handle the weight reduction that comes from creating an offspring.
+        If the weight reduction is survivable for the mother,
+        the weight is reduced by this amount and a True-value is returned.
+
+        Parameters
+        ----------
+        newborn_weight: float
+            the weight of a possible offspring
+
+        Returns
+        -------
+        binary: boolean
+            True or False based on condition checks
+        """
         if self.weight >= self.params['xi'] * newborn_weight:
             # Reduce weight of mother by the weight of newborn * xi
             self.weight -= self.params['xi'] * newborn_weight
@@ -187,17 +227,40 @@ class Animal:
             return False
 
     def create_newborn(self, num_animals):
+        """
+        Creates a new animal based on several condition checks. If all checks passed:
+        create a new animal of the same class as mother-animal, and return the newborn object.
+        Parameters
+        ----------
+        num_animals: int
+            Number of animals of same species in current cell
+
+        Returns
+        -------
+        newborn: object
+            if no newborn object is created, returns None
+
+        """
         if self.check_mating_weight_conditions(num_animals):
             prob = min(1, self.params['gamma'] * self.calculate_fitness() * (num_animals - 1))
             creating = self.from_prob_to_binary(prob)
             if creating:
-                newborn = self.__class__()  # OBS HOW TO MAKE THE ANIMAL OBJECT OF CORRECT SPECIES
+                newborn = self.__class__()
                 if self.check_mother_minus_newborn_weight_conditions(newborn.weight):
                     return newborn
-            else:
-                return None
+        return None
 
     def is_dying(self):
+        """
+        Checks if animal object is dying based on probability calculated form formula:
+        p = omega ( 1 - fitness)
+        It weight is zero, the animal will die.
+
+        Returns
+        -------
+        binary: boolean
+            True if animal must die, False if not.
+        """
         if self.weight <= 0:
             return True
         prob = self.params['omega'] * (1 - self.calculate_fitness())
@@ -205,6 +268,16 @@ class Animal:
         return self.from_prob_to_binary(prob)
 
     def check_if_migrates(self):
+        """
+        Checks if an animal will migrate. The animal can only migrate once a year.
+        Probability of migration is calculated from formula p = mu * fitness
+
+        Returns
+        -------
+        binary: boolean
+            Returns True if animal migrates, False if not.
+
+        """
         if self.has_migrated_this_year:
             return False
 
@@ -217,25 +290,46 @@ class Animal:
         else:
             return False
 
+    @staticmethod
+    def raise_non_valid_attribute(attribute_name, attribute):
+        """
+        Raise an error if the input is of invalid type
+
+        Parameters
+        ----------
+        attribute_name : str
+        attribute : value of the attribute
+        """
+        if not isinstance(attribute, (int, float)):
+            raise ValueError(attribute_name + ' of animal can\'t be set to '
+                             + attribute_name +
+                             ', it has to be integer or float')
 
     def get_weight(self):
+        """
+        Gets the weight of an animal object
+
+        Returns
+        -------
+        weight: float
+        """
         return self.weight
-    #
-    # def get_age(self):
-    #     return self.age
 
-    # def get_fitness(self):
-    #     return self.fitness
-
-    def get_F(self):
+    def get_param_f(self):
+        """
+        Gets the parameter value F for animal object
+        Returns
+        -------
+        parameter value F: float
+        """
         return self.params['F']
-
-    # def get_initial_weight(self):
-    #     return self.initial_weight()
 
 
 class Herbivore(Animal):
-    # HERBIVORE PARAMS HERE
+    """
+    Herbivore subclass of Animal Base Class
+    """
+
     params = {'w_birth': 8.,
               'sigma_birth': 1.5,
               'beta': 0.9,
@@ -253,7 +347,7 @@ class Herbivore(Animal):
 
     def __init__(self, age=None, weight=None):
         """
-        subclass of Animal class.
+        Constructor for the Herbivore subclass of Animal base class.
         Parameters
         ----------
         age: int
@@ -261,9 +355,35 @@ class Herbivore(Animal):
         """
         super().__init__(age, weight)
 
+    def eat(self, amount=None):
+        """
+        Eat method for Herbivores.
+        Herbivores eat a specific amount.
+        If not specified, its value will be chosen based on the standard parameters for herbivore
+        class. When the animal eats, the weight is updated accordingly:
+        weight change = beta * amount.
+
+        Parameters
+        ----------
+        amount : float
+
+        Returns
+        -------
+        amount of food the herbivore consumed.
+
+        """
+        if amount is None:
+            amount = self.params['F']
+
+        self.weight += self.params['beta'] * amount
+        return amount
+
 
 class Carnivore(Animal):
-    # CARNIVORE params
+    """
+    Carnivore subclass of Animal Base Class
+    """
+
     params = {'w_birth': 6.,
               'sigma_birth': 1.0,
               'beta': 0.75,
@@ -282,7 +402,8 @@ class Carnivore(Animal):
 
     def __init__(self, age=None, weight=None):
         """
-        subclass of Animal class.
+        Constructor for the Carnivore subclass of Animal base class.
+
         Parameters
         ----------
         age: int
@@ -290,9 +411,24 @@ class Carnivore(Animal):
         """
         super().__init__(age, weight)
 
-    # Carnivore eating method
-
     def check_carn_prey(self, herb_fitness, carn_fitness):
+        """
+        Check assessing if a carnivore can prey on a herbivore.
+        Condition is based on both animals fitness.
+        Probability of carnivore hunting the herbivore is:
+        p = (carnivore_fitness - herbivore_fitness ) DeltaPhiMax,
+        where deltaPhiMax is given in the Params dictionary
+
+        Parameters
+        ----------
+        herb_fitness: float
+        carn_fitness: float
+
+        Returns
+        -------
+        binary: boolean
+            Returns True if carnivore can prey on herbivore, False if not.
+        """
 
         if carn_fitness <= herb_fitness:
             return False
@@ -305,6 +441,24 @@ class Carnivore(Animal):
             return True
 
     def eat(self, herb_list):
+        """
+        Eat method for Carnivores.
+        Loops through a list of all available herbivores, checks if the carnivore preys on it.
+        If the carnivore eats the herbivore, its appended to the eaten_herbs list, to be returned.
+        The weight and fitness is updates for every animal the carnivore eats, and the
+        carnivore never eats more than the appetite parameter F, set in the Params dictionary.
+
+        Parameters
+        ----------
+        herb_list: list
+            list of all available herbivores in cell
+
+        Returns
+        -------
+        eaten_herbs: List
+            List of all the eaten herbivores.
+            List is returned empty if no Herbivore were eaten by the Carnivore
+        """
         eaten_herbs = []
         remaining_appetite = self.params['F']
 
@@ -321,20 +475,7 @@ class Carnivore(Animal):
                 remaining_appetite -= weight_to_eat
                 self.weight += self.params['beta'] * weight_to_eat
 
-            if remaining_appetite == 0:  #if not remaining appetite
+            if remaining_appetite == 0:  # if no remaining appetite
                 return eaten_herbs
 
         return eaten_herbs
-
-
-
-# if __name__ == "__main__":
-#     num_animals=2
-#     h = Animal()
-#     print(h.age)
-#     print(h.weight)
-#     print(h.fitness)
-#     for i in range(30):
-#         h.eat()
-#     # h.if_create_new_born(num_animals)
-#     print(h.check_if_will_create_newborn(num_animals, 10))
